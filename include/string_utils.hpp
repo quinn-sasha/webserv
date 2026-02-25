f2836355261e069928edc828c7eb9c7226523d1d
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2026-02-25 10:39:35 +0900

    Added split_string()

--- include/string_utils.hpp
+++ include/string_utils.hpp
@@ -1,8 +1,14 @@
 #ifndef INCLUDE_STRING_UTILS_HPP_
 #define INCLUDE_STRING_UTILS_HPP_
 
+#include <list>
 #include <string>
 
 std::string to_lower(std::string s);
+
+std::list<std::string> split_string(const std::string& target,
+                                    const std::string& delimiter);
+
+std::string trim(const std::string& target, std::string to_delete);
 
 #endif  // INCLUDE_STRING_UTILS_HPP_
--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <list>
 #include <string>
 
 std::string to_lower(std::string s) {
@@ -5,4 +7,34 @@ std::string to_lower(std::string s) {
     s[i] = std::tolower(static_cast<unsigned char>(s[i]));
   }
   return s;
+}
+
+std::list<std::string> split_string(const std::string& target,
+                                    const std::string& delimiter) {
+  std::list<std::string> result;
+  if (target.empty()) {
+    return result;
+  }
+  std::size_t start = 0;
+  while (true) {
+    std::size_t word_end = target.find(delimiter, start);
+    if (word_end == std::string::npos) {
+      result.push_back(target.substr(start));
+      return result;
+    }
+    result.push_back(target.substr(start, word_end - start));
+    start = word_end + delimiter.size();
+  }
+}
+
+std::string trim(const std::string& target, std::string to_delete) {
+  if (target.empty()) {
+    return "";
+  }
+  std::size_t start = target.find_first_not_of(to_delete);
+  std::size_t last = target.find_last_not_of(to_delete);
+  if (start == std::string::npos) {
+    return "";
+  }
+  return target.substr(start, last - start + 1);
 }
