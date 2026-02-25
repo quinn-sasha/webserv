f73df2525ef61051c1d80cc59f6bcd3749d64666
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2026-02-25 16:37:09 +0900

    Added BodyParseInfo structure inside of Request

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_PARSER_HPP_
 #define INCLUDE_PARSER_HPP_
 
+#include <list>
 #include <map>
 #include <string>
 
@@ -41,6 +42,14 @@ struct Request {
   std::string target;
   HttpVersion version;
   std::map<std::string, std::string> headers;
+
+  struct BodyParseInfo {
+    bool is_chunked;
+    std::list<std::string> encodings;
+    std::size_t content_length;
+  } body_parse_info;
+
+  std::string body;
 };
 
 class Parser {
@@ -57,16 +66,16 @@ class Parser {
   ParserStatus parse_method_name(const std::string& method);
   ParserStatus parse_request_target(const std::string& target);
   ParserStatus parse_http_version(const std::string& version);
-  ParserStatus parse_transfer_encodings(const std::string& field_value) const;
+  ParserStatus parse_transfer_encodings(const std::string& field_value);
+  ParserStatus parse_request_line(const std::string& start_line);
+  ParserStatus parse_field_line(const std::string& field_line);
+  ParserStatus determine_next_action();
   // Prohibit copy and assignment
   Parser(const Parser& ohter);
   Parser& operator=(const Parser& ohter);
 
  public:
   Parser() : state_(kParsingRequestLine) {}
-  ParserStatus parse_request_line(const std::string& start_line);
-  ParserStatus parse_field_line(const std::string& field_line);
-  ParserStatus determine_next_action() const;
   ParserStatus parse_request(const char* message, ssize_t num_read);
   const Request& get_request() const { return request_; }
 };
