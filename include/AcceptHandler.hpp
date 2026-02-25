be5767364f53e068a2c9452a47cb72286d813f8e
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2026-02-25 10:36:28 +0900

    Modified: ajuested change of status name to AcceptHandler

--- include/AcceptHandler.hpp
+++ include/AcceptHandler.hpp
@@ -16,8 +16,10 @@ class AcceptHandler : public MonitoredFdHandler {
   AcceptHandler(int listen_fd, Server& server);
   ~AcceptHandler() {}  // Do nothing because ListenSocket will close fd
   HandlerStatus handle_input();  // Return kContinue or kFatalError
-  HandlerStatus handle_output() { return kFatalError; }  // Must not call this
-  HandlerStatus handle_poll_error() { return kFatalError; }
+  HandlerStatus handle_output() {
+    return kHandlerFatalError;
+  }  // Must not call this
+  HandlerStatus handle_poll_error() { return kHandlerFatalError; }
 };
 
 #endif  // INCLUDE_ACCEPTHANDLER_HPP_
