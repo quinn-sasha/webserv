5c93d44ea27958a5b44cba07203cb8920781b3a8
kota1717 <kota03017@icloud.com>
2026-03-17 18:44:05 +0900

    引数でconfigファイルを渡してないときでもデフォルトで設定されるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,20 @@
 #include "signal_utils.hpp"
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
+  std::string config_path;
+
+  if (argc == 2) {
+    config_path = argv[1];
+  } else if (argc = 1) {
+    config_path = "test/default.conf";
+  } else {
     error_exit("Usage: ./webserv configuration_file");
   }
   set_signal_handler(SIGINT, turn_off_running_status);
   set_signal_handler(SIGTERM, turn_off_running_status);
   try {
     std::vector<ListenConfig> listen_configs;
-    Server server(argv[1]);
+    Server server(config_path);
     server.run();
   } catch (const std::exception& e) {
     std::cout << e.what() << "\n";
